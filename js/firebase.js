bb7b4c586d09caeb9323cee7ac70094178dbfe55
zearn-app <zoroz0673@gmail.com>
2026-06-22 23:19:14 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+getAuth
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+const auth=
+getAuth(app);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAXtM0DnYPTYbdQmvv93KAQwcqxty2C1vQ",
-  authDomain: "kodaihillsspot-4a1b8.firebaseapp.com",
-  projectId: "kodaihillsspot-4a1b8",
-  storageBucket: "kodaihillsspot-4a1b8.firebasestorage.app",
-  messagingSenderId: "396566428046",
-  appId: "1:396566428046:web:c9bafa2143b34e7d64ccdf",
-  measurementId: "G-BZ8MBZEJQ4"
+export{
+db,
+auth
 };
-
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-const db = getFirestore(app);
-
-export {auth,db};
