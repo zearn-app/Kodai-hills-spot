0e998d5dd63dc83be1d3838702b73deac3042f2d
zearn-app <zoroz0673@gmail.com>
2026-06-23 07:38:48 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,62 +1,63 @@
-// Firebase imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {initializeApp}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
+
 getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
 RecaptchaVerifier,
 signInWithPhoneNumber
+
 }
+
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
+
 getFirestore,
 doc,
 setDoc,
-collection,
-query,
-where,
-getDocs
+getDoc
+
 }
+
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-
-
-// FIREBASE CONFIG
 
 const firebaseConfig={
 
 apiKey:"AIzaSyAXtM0DnYPTYbdQmvv93KAQwcqxty2C1vQ",
 
-authDomain:"kodaihillsspot-4a1b8.firebaseapp.com",
+authDomain:
+"kodaihillsspot-4a1b8.firebaseapp.com",
 
-projectId:"kodaihillsspot-4a1b8",
+projectId:
+"kodaihillsspot-4a1b8",
 
-storageBucket:"kodaihillsspot-4a1b8.firebasestorage.app",
+storageBucket:
+"kodaihillsspot-4a1b8.firebasestorage.app",
 
-messagingSenderId:"396566428046",
+messagingSenderId:
+"396566428046",
 
-appId:"1:396566428046:web:c9bafa2143b34e7d64ccdf"
+appId:
+"1:396566428046:web:c9bafa2143b34e7d64ccdf"
 
 };
 
 
-// Initialize Firebase
+const app=
+initializeApp(
+firebaseConfig
+);
 
-const app=initializeApp(firebaseConfig);
+const auth=
+getAuth(app);
 
-const auth=getAuth(app);
+const db=
+getFirestore(app);
 
-const db=getFirestore(app);
-
-
-// Recaptcha
-
-window.onload=()=>{
 
 window.recaptchaVerifier=
-
 new RecaptchaVerifier(
 
 auth,
@@ -71,14 +72,15 @@ size:"normal"
 
 );
 
-};
+
+let currentUser=null;
 
 
+/* SEND OTP */
 
-// SIGNUP
-
-document.getElementById(
-"signupBtn"
+document
+.getElementById(
+"sendOtpBtn"
 )
 
 .onclick=
@@ -86,224 +88,40 @@ document.getElementById(
 async()=>{
 
 try{
-
-const name=
-document.getElementById(
-"name"
-).value.trim();
 
 const phone=
-document.getElementById(
+
+"+91"+
+
+document
+.getElementById(
 "phone"
-).value.trim();
-
-const email=
-document.getElementById(
-"email"
-).value.trim();
-
-const password=
-document.getElementById(
-"password"
-).value.trim();
-
-
-if(
-
-!name||
-!phone||
-!email||
-!password
-
-){
-
-alert(
-"Fill all fields"
-);
-
-return;
-
-}
-
-
-const userCredential=
-
-await createUserWithEmailAndPassword(
-
-auth,
-email,
-password
-
-);
-
-
-const user=
-userCredential.user;
-
-
-await setDoc(
-
-doc(
-db,
-"Users",
-user.uid
-),
-
-{
-
-uid:user.uid,
-
-name:name,
-
-phone:"+91"+phone,
-
-email:email
-
-}
-
-);
-
-
-alert(
-"Signup successful"
-);
-
-location.reload();
-
-}
-
-catch(error){
-
-alert(
-error.message
-);
-
-}
-
-};
-
-
-
-
-// LOGIN + SEND OTP
-
-
-document.getElementById(
-"loginBtn"
 )
 
-.onclick=
+.value.trim();
 
-async()=>{
-
-
-try{
-
-const email=
-document.getElementById(
-"loginEmail"
-).value.trim();
-
-const password=
-document.getElementById(
-"loginPassword"
-).value.trim();
-
-
-if(
-
-!email||
-!password
-
-){
-
-alert(
-"Enter email and password"
-);
-
-return;
-
-}
-
-
-await signInWithEmailAndPassword(
-
-auth,
-email,
-password
-
-);
-
-
-
-const q=
-
-query(
-
-collection(
-db,
-"Users"
-),
-
-where(
-"email",
-"==",
-email
-)
-
-);
-
-
-const snapshot=
-
-await getDocs(q);
-
-
-let phone="";
-
-
-snapshot.forEach(item=>{
-
-phone=
-item.data().phone;
-
-});
-
-
-const appVerifier=
-
-window.recaptchaVerifier;
-
-
-const confirmationResult=
+const result=
 
 await signInWithPhoneNumber(
 
 auth,
 phone,
-appVerifier
+window.recaptchaVerifier
 
 );
 
-
 window.confirmationResult=
+result;
 
-confirmationResult;
+document
+.getElementById(
+"otpBox"
+)
 
-
-// SHOW OTP
-
-document.getElementById(
-"otp"
-).style.display=
-"block";
-
-
-document.getElementById(
-"verifyOtpBtn"
-).style.display=
-"block";
-
+.classList
+.remove(
+"hidden"
+);
 
 alert(
 "OTP Sent"
@@ -313,10 +131,12 @@ alert(
 
 catch(error){
 
-console.log(error);
-
 alert(
 error.message
+);
+
+console.log(
+error
 );
 
 }
@@ -324,71 +144,76 @@ error.message
 };
 
 
+/* VERIFY OTP */
 
-
-// VERIFY OTP
-
-document.getElementById(
-"verifyOtpBtn"
+document
+.getElementById(
+"verifyBtn"
 )
 
 .onclick=
 
 async()=>{
 
-
 try{
 
 const otp=
 
-document.getElementById(
+document
+.getElementById(
 "otp"
-).value.trim();
+)
 
+.value;
 
-if(!otp){
+const result=
 
-alert(
-"Enter OTP"
-);
-
-return;
-
-}
-
-
-await window.confirmationResult.confirm(
+await window
+.confirmationResult
+.confirm(
 otp
 );
 
+currentUser=
+result.user;
 
-alert(
-"Login Success"
+const snapshot=
+
+await getDoc(
+
+doc(
+db,
+"Users",
+currentUser.uid
+)
+
 );
 
 
-// Admin redirect
-
 if(
 
-auth.currentUser.email===
-
-"kodaihillsspot@gmail.com"
+snapshot.exists()
 
 ){
 
 window.location=
-"admin.html";
+"home.html";
 
 }
 
 else{
 
-window.location=
-"index.html";
+document
+.getElementById(
+"detailsBox"
+)
+
+.classList
+.remove(
+"hidden"
+);
 
 }
-
 
 }
 
@@ -399,5 +224,65 @@ alert(
 );
 
 }
+
+};
+
+
+/* SAVE DETAILS */
+
+document
+.getElementById(
+"saveBtn"
+)
+
+.onclick=
+
+async()=>{
+
+const name=
+
+document
+.getElementById(
+"name"
+)
+
+.value;
+
+const email=
+
+document
+.getElementById(
+"email"
+)
+
+.value;
+
+
+await setDoc(
+
+doc(
+db,
+"Users",
+currentUser.uid
+),
+
+{
+
+uid:
+currentUser.uid,
+
+name:name,
+
+email:email,
+
+phone:
+currentUser.phoneNumber
+
+}
+
+);
+
+window.location=
+"home.html";
 
 };
