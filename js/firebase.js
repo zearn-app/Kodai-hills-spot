205c6481a44231c73c611fff62fc14529e989150
zearn-app <zoroz0673@gmail.com>
2026-06-23 13:32:22 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,39 +1,55 @@
 import {
+initializeApp
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
 getAuth
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+import {
+getFirestore
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+const firebaseConfig={
+
+apiKey:"AIzaSyAXtM0DnYPTYbdQmvv93KAQwcqxty2C1vQ",
+
+authDomain:"kodaihillsspot-4a1b8.firebaseapp.com",
+
+projectId:"kodaihillsspot-4a1b8",
+
+storageBucket:"kodaihillsspot-4a1b8.firebasestorage.app",
+
+messagingSenderId:"396566428046",
+
+appId:"1:396566428046:web:c9bafa2143b34e7d64ccdf",
+
+measurementId:"G-BZ8MBZEJQ4"
+
+};
+
+
+const app=
+initializeApp(
+firebaseConfig
+);
 
 const auth=
-getAuth(app);
+getAuth(
+app
+);
 
-export{
-db,
-auth
+const db=
+getFirestore(
+app
+);
+
+
+export {
+auth,
+db
 };
-
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAXtM0DnYPTYbdQmvv93KAQwcqxty2C1vQ",
-  authDomain: "kodaihillsspot-4a1b8.firebaseapp.com",
-  projectId: "kodaihillsspot-4a1b8",
-  storageBucket: "kodaihillsspot-4a1b8.firebasestorage.app",
-  messagingSenderId: "396566428046",
-  appId: "1:396566428046:web:c9bafa2143b34e7d64ccdf",
-  measurementId: "G-BZ8MBZEJQ4"
-};
-
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-const db = getFirestore(app);
-
-export {auth,db};
