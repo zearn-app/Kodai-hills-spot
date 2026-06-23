c99ac55859dc754b6f75d933a90d8753d146af0a
zearn-app <zoroz0673@gmail.com>
2026-06-23 08:28:17 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,250 +2,156 @@ import { initializeApp }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
+
 getAuth,
-RecaptchaVerifier,
-signInWithPhoneNumber,
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword
+
 }
+
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+
 import {
+
 getFirestore,
 doc,
-setDoc,
-getDoc
+setDoc
+
 }
+
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 
 
 const firebaseConfig={
 
-apiKey:"YOUR_API_KEY",
-authDomain:"kodaihillsspot-4a1b8.firebaseapp.com",
-projectId:"kodaihillsspot-4a1b8",
-storageBucket:"kodaihillsspot-4a1b8.firebasestorage.app",
-messagingSenderId:"396566428046",
-appId:"1:396566428046:web:c9bafa2143b34e7d64ccdf"
+apiKey:"AIzaSyAXtM0DnYPTYbdQmvv93KAQwcqxty2C1vQ",
 
-};
+authDomain:
+"kodaihillsspot-4a1b8.firebaseapp.com",
 
-const app=initializeApp(firebaseConfig);
+projectId:
+"kodaihillsspot-4a1b8",
 
-const auth=getAuth(app);
+storageBucket:
+"kodaihillsspot-4a1b8.firebasestorage.app",
 
-const db=getFirestore(app);
+messagingSenderId:
+"396566428046",
 
-let currentUser;
-
-
-/* RECAPTCHA */
-
-window.recaptchaVerifier=
-new RecaptchaVerifier(
-auth,
-"recaptcha-container",
-{
-size:"normal"
-}
-);
-
-window.recaptchaVerifier.render();
-
-
-
-/* PHONE OTP */
-
-document.getElementById(
-"sendOtpBtn"
-).onclick=async()=>{
-
-try{
-
-const number=
-document.getElementById(
-"phone"
-).value.trim();
-
-const phone=
-"+91"+number;
-
-const result=
-await signInWithPhoneNumber(
-auth,
-phone,
-window.recaptchaVerifier
-);
-
-window.confirmationResult=
-result;
-
-document.getElementById(
-"otpBox"
-).classList.remove(
-"hidden"
-);
-
-alert(
-"OTP sent"
-);
-
-}
-
-catch(error){
-
-alert(
-error.message
-);
-
-console.log(error);
-
-}
+appId:
+"1:396566428046:web:c9bafa2143b34e7d64ccdf"
 
 };
 
 
+const app=
+initializeApp(
+firebaseConfig
+);
 
-/* VERIFY OTP */
+const auth=
+getAuth(app);
 
-document.getElementById(
-"verifyBtn"
-).onclick=async()=>{
+const db=
+getFirestore(app);
 
-const otp=
-document.getElementById(
-"otp"
-).value;
 
-try{
 
-const result=
-await window
-.confirmationResult
-.confirm(otp);
+/* SIGNUP */
 
-currentUser=
-result.user;
-
-const userDoc=
-await getDoc(
-doc(
-db,
-"Users",
-currentUser.uid
+document
+.getElementById(
+"signupBtn"
 )
-);
 
-if(userDoc.exists()){
+.onclick=
 
-window.location=
-"home.html";
+async()=>{
 
-}
-
-else{
-
-document.getElementById(
-"detailsBox"
-).classList.remove(
-"hidden"
-);
-
-}
-
-}
-
-catch{
-
-alert(
-"Wrong OTP"
-);
-
-}
-
-};
-
-
-
-/* SAVE NEW PHONE USER */
-
-document.getElementById(
-"saveBtn"
-).onclick=async()=>{
+try{
 
 const name=
-document.getElementById(
+document
+.getElementById(
 "name"
-).value;
+)
+.value.trim();
 
 const email=
-document.getElementById(
-"newEmail"
-).value;
+document
+.getElementById(
+"email"
+)
+.value.trim();
+
+const password=
+document
+.getElementById(
+"password"
+)
+.value.trim();
+
+
+if(
+!name||
+!email||
+!password
+){
+
+alert(
+"Fill all fields"
+);
+
+return;
+
+}
+
+
+const userCredential=
+
+await createUserWithEmailAndPassword(
+
+auth,
+email,
+password
+
+);
+
+
+const user=
+userCredential.user;
+
 
 await setDoc(
+
 doc(
 db,
 "Users",
-currentUser.uid
+user.uid
 ),
+
 {
-uid:currentUser.uid,
+
+uid:user.uid,
 name:name,
-email:email,
-phone:currentUser.phoneNumber
+email:email
+
 }
+
+);
+
+
+alert(
+"Signup successful"
 );
 
 window.location=
 "home.html";
 
-};
-
-
-
-/* EMAIL SIGNUP */
-
-document.getElementById(
-"signupBtn"
-).onclick=async()=>{
-
-try{
-
-const email=
-document.getElementById(
-"email"
-).value;
-
-const password=
-document.getElementById(
-"password"
-).value;
-
-const result=
-await createUserWithEmailAndPassword(
-auth,
-email,
-password
-);
-
-await setDoc(
-doc(
-db,
-"Users",
-result.user.uid
-),
-{
-uid:result.user.uid,
-email:email
 }
-);
 
-alert(
-"Account created"
-);
-
-}
 catch(error){
 
 alert(
@@ -258,34 +164,51 @@ error.message
 
 
 
-/* EMAIL LOGIN */
+/* LOGIN */
 
-document.getElementById(
+document
+.getElementById(
 "loginBtn"
-).onclick=async()=>{
+)
+
+.onclick=
+
+async()=>{
 
 try{
 
 const email=
-document.getElementById(
-"email"
-).value;
+document
+.getElementById(
+"loginEmail"
+)
+.value.trim();
 
 const password=
-document.getElementById(
-"password"
-).value;
+document
+.getElementById(
+"loginPassword"
+)
+.value.trim();
+
 
 await signInWithEmailAndPassword(
+
 auth,
 email,
 password
+
+);
+
+alert(
+"Login successful"
 );
 
 window.location=
 "home.html";
 
 }
+
 catch(error){
 
 alert(
