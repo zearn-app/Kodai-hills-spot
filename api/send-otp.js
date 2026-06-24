b23abfa2bf8309256b01db1da17154b0c0d3a606
zearn-app <zoroz0673@gmail.com>
2026-06-24 23:32:47 +0530

    Update send-otp.js

--- api/send-otp.js
+++ api/send-otp.js
@@ -11,45 +11,27 @@ try{
 
 const {phone}=req.body;
 
-if(!phone){
-return res.status(400).json({
-
-success:false,
-message:"Phone number missing"
-
-});
-
-}
-
 await client.verify.v2
 .services(
 process.env.VERIFY_SERVICE
 )
 .verifications
 .create({
-
 to:"+91"+phone,
 channel:"sms"
-
 });
 
 return res.status(200).json({
-
-success:true,
-message:"OTP Sent"
-
+success:true
 });
 
-}
-catch(error){
+}catch(error){
 
 console.log(error);
 
 return res.status(500).json({
-
 success:false,
 message:error.message
-
 });
 
 }
