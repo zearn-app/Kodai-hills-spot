dbf5f5ad2f0d58d0cb31b3c162f4cb266654bf24
zearn-app <zoroz0673@gmail.com>
2026-06-24 22:37:53 +0530

    Create send-otp.js

---
+++ js/send-otp.js
@@ -0,0 +1,38 @@
+import twilio from "twilio";
+
+const client = twilio(
+process.env.TWILIO_SID,
+process.env.TWILIO_AUTH
+);
+
+export default async function handler(req,res){
+
+const {phone}=req.body;
+
+try{
+
+await client.verify.v2
+.services(
+process.env.VERIFY_SERVICE
+)
+.verifications
+.create({
+to:"+91"+phone,
+channel:"sms"
+});
+
+res.json({
+success:true
+});
+
+}
+catch(error){
+
+res.json({
+success:false,
+message:error.message
+});
+
+}
+
+}
