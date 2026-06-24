656e6cae4b30ad291cf846a5e6bace369ebe25b0
zearn-app <zoroz0673@gmail.com>
2026-06-24 22:38:38 +0530

    Create verify-otp.js

---
+++ js/verify-otp.js
@@ -0,0 +1,38 @@
+import twilio from "twilio";
+
+const client = twilio(
+process.env.TWILIO_SID,
+process.env.TWILIO_AUTH
+);
+
+export default async function handler(req,res){
+
+const {phone,otp}=req.body;
+
+try{
+
+const result=
+await client.verify.v2
+.services(
+process.env.VERIFY_SERVICE
+)
+.verificationChecks
+.create({
+to:"+91"+phone,
+code:otp
+});
+
+res.json({
+verified:result.status==="approved"
+});
+
+}
+catch(error){
+
+res.json({
+verified:false
+});
+
+}
+
+}
